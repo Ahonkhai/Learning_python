f552f386e30937e0b28ab44441fbc51efc6be6de
Ahonkhai <kakashiblack8@gmail.com>
2025-03-23 20:12:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ print("i'm hungry!")
 # Tested simple print
 
 # String
-name = "David"
+first_name = "David"
+last_name = "Ahonkhai"
+full_name = first_name + " " + last_name
 
-print ("my name is " + name )
+print ("My name is " + full_name )
 
 # Check datatype of variable
 
-print(type(name))
+print(type(first_name))
