01d302fe22c4febddcfc66039beeb57845b3cc44
Ahonkhai <kakashiblack8@gmail.com>
2025-03-27 03:20:48 +0100

    Day 2

---
+++ second.py
@@ -0,0 +1,30 @@
+#  Day 2
+
+#  Subscripting
+print("hello"[0])
+print(len("12345"))
+
+# Check data type
+print(type(4))
+print(type("David"))
+print(type(True))
+print(type(2.5))
+
+# Type conversion
+
+print("12345") #String
+print(int("12345")) #int 
+
+name = len(input("Enter your name"))
+print("Number of letter in your name: ")
+
+print(int(name))
+
+# Mathermatical operators
+
+print(123 + 456)
+print(8 - 4)
+print(3 * 2)
+print(6 / 2)
+print(6 / 7) # Removes decimal
+print(2 ** 2) # basically 2²   
